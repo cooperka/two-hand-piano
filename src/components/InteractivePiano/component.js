d1df2db3260f1a3bc9dba3fccf6651b38b6af24a
Kevin Cooper <cooperka92@gmail.com>
2019-12-27 16:11:41 -0800

    :rocket: Make keyMap stateful

--- src/components/InteractivePiano/component.js
+++ src/components/InteractivePiano/component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Piano from 'react-piano-component';
 
 import PianoKey from './PianoKey/component';
@@ -6,7 +6,36 @@ import PianoSettings from './PianoSettings/component';
 
 import './styles.css';
 
+const defaultKeyMap = {
+  Q: 'C4',
+  2: 'C#4',
+  W: 'D4',
+  3: 'D#4',
+  E: 'E4',
+  R: 'F4',
+  5: 'F#4',
+  T: 'G4',
+  6: 'G#4',
+  Y: 'A4',
+  7: 'A#4',
+  U: 'B4',
+  V: 'C5',
+  G: 'C#5',
+  B: 'D5',
+  H: 'D#5',
+  N: 'E5',
+  M: 'F5',
+  K: 'F#5',
+  ',': 'G5',
+  L: 'G#5',
+  '.': 'A5',
+  ';': 'A#5',
+  '/': 'B5',
+};
+
 function InteractivePiano() {
+  const [keyMap, setKeyMap] = useState(defaultKeyMap);
+
   return (
     <div>
       <PianoSettings />
@@ -18,32 +47,7 @@ function InteractivePiano() {
           startNote="C3"
           endNote="C6"
           renderPianoKey={PianoKey}
-          keyboardMap={{
-            Q: 'C4',
-            2: 'C#4',
-            W: 'D4',
-            3: 'D#4',
-            E: 'E4',
-            R: 'F4',
-            5: 'F#4',
-            T: 'G4',
-            6: 'G#4',
-            Y: 'A4',
-            7: 'A#4',
-            U: 'B4',
-            V: 'C5',
-            G: 'C#5',
-            B: 'D5',
-            H: 'D#5',
-            N: 'E5',
-            M: 'F5',
-            K: 'F#5',
-            ',': 'G5',
-            L: 'G#5',
-            '.': 'A5',
-            ';': 'A#5',
-            '/': 'B5',
-          }}
+          keyboardMap={keyMap}
         />
       </div>
     </div>
