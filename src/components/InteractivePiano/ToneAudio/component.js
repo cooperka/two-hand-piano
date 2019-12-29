0b39c16465513c8efd48081b7273a45ac35fa07b
Kevin Cooper <cooperka92@gmail.com>
2019-12-29 12:46:26 -0800

    :sparkles: Use Salamander sample notes
    Saves ~0.5 MB compared to default MP3 data

--- src/components/InteractivePiano/ToneAudio/component.js
+++ src/components/InteractivePiano/ToneAudio/component.js
@@ -3,10 +3,47 @@ import isEqual from 'lodash/isEqual';
 import difference from 'lodash/difference';
 import Tone from 'tone';
 
+const sampleData = {
+  A0: 'A0.[mp3|ogg]',
+  C1: 'C1.[mp3|ogg]',
+  'D#1': 'Ds1.[mp3|ogg]',
+  'F#1': 'Fs1.[mp3|ogg]',
+  A1: 'A1.[mp3|ogg]',
+  C2: 'C2.[mp3|ogg]',
+  'D#2': 'Ds2.[mp3|ogg]',
+  'F#2': 'Fs2.[mp3|ogg]',
+  A2: 'A2.[mp3|ogg]',
+  C3: 'C3.[mp3|ogg]',
+  'D#3': 'Ds3.[mp3|ogg]',
+  'F#3': 'Fs3.[mp3|ogg]',
+  A3: 'A3.[mp3|ogg]',
+  C4: 'C4.[mp3|ogg]',
+  'D#4': 'Ds4.[mp3|ogg]',
+  'F#4': 'Fs4.[mp3|ogg]',
+  A4: 'A4.[mp3|ogg]',
+  C5: 'C5.[mp3|ogg]',
+  'D#5': 'Ds5.[mp3|ogg]',
+  'F#5': 'Fs5.[mp3|ogg]',
+  A5: 'A5.[mp3|ogg]',
+  C6: 'C6.[mp3|ogg]',
+  'D#6': 'Ds6.[mp3|ogg]',
+  'F#6': 'Fs6.[mp3|ogg]',
+  A6: 'A6.[mp3|ogg]',
+  C7: 'C7.[mp3|ogg]',
+  'D#7': 'Ds7.[mp3|ogg]',
+  'F#7': 'Fs7.[mp3|ogg]',
+  A7: 'A7.[mp3|ogg]',
+  C8: 'C8.[mp3|ogg]',
+};
+
 class ToneAudio extends Component {
   constructor(props) {
     super(props);
-    this.synth = new Tone.PolySynth(4, Tone.Synth).toMaster();
+
+    this.sampler = new Tone.Sampler(sampleData, {
+      release: 1,
+      baseUrl: 'https://tonejs.github.io/examples/audio/salamander/',
+    }).toMaster();
   }
 
   componentDidMount() {
@@ -28,11 +65,11 @@ class ToneAudio extends Component {
   }
 
   startPlayingNotes(startedNotes) {
-    this.synth.triggerAttack(startedNotes);
+    this.sampler.triggerAttack(startedNotes);
   }
 
   stopPlayingNotes(stoppedNotes) {
-    this.synth.triggerRelease(stoppedNotes);
+    this.sampler.triggerRelease(stoppedNotes);
   }
 
   render() {
